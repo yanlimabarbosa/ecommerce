ba0af28fe88e90934415dbff66f47b98e41b26b9
Yan Lima Barbosa <yanbr763@gmail.com>
2025-09-24 22:47:09 -0300

    feat: implement API Gateway server with middleware and health check endpoint
    - Added main server file for the API Gateway using Express.
    - Configured CORS, logging, request parsing, and rate limiting middleware.
    - Implemented a health check endpoint at `/gateway-health`.
    - Set up proxying to a backend service running on localhost:6001.

---
+++ apps/api-gateway/src/main.ts
@@ -0,0 +1,47 @@
+import express from "express"
+import cors from "cors"
+import proxy from "express-http-proxy"
+import morgan from "morgan"
+import rateLimit from "express-rate-limit"
+import swaggerUi from "swagger-ui-express"
+import axios from "axios"
+import cookieParser from "cookie-parser"
+import { Request } from 'express';
+
+const app = express()
+
+app.use(cors({
+  origin: ["http://localhost:3000"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
+}))
+
+app.use(morgan("dev"))
+app.use(express.json({ limit:"100mb" }))
+app.use(express.urlencoded({ limit: "100mb", extended: true }))
+app.use(cookieParser())
+app.set("trust proxy", 1)
+
+// Rate limiting
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: (req: Request) => (req.user? 1000 : 100), // limit each IP to 100 requests per windowMs
+  message: "Too many requests from this IP, please try again later.",
+  standardHeaders: true,
+  legacyHeaders: true,
+  keyGenerator: (req: Request) => req.ip ?? 'unknown', 
+})
+
+app.use(limiter)
+
+app.get("/gateway-health", (_, res) => {
+  res.send({ message: "Welcome to api-gateway!" })
+})
+
+app.use("/", proxy("http://localhost:6001"))
+
+const port = process.env.PORT || 8080
+const server = app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}/api`)
+})
+server.on("error", console.error)
