e450715c4213f644627fd3828ac26786f6560b7f
Yan Lima Barbosa <yanbr763@gmail.com>
2025-09-30 00:35:27 -0300

    refactor: clean up main.ts for improved readability
    - Reorganized import statements for consistency.
    - Enhanced formatting of middleware and route definitions for better clarity.
    - Standardized the use of semicolons and indentation throughout the file.

--- apps/auth-service/src/main.ts
+++ apps/auth-service/src/main.ts
@@ -1,30 +1,32 @@
-import express from 'express';
-import cors from "cors"
-import { errorMiddleware } from "../../../packages/error-handler/error-middleware"
 import cookieParser from "cookie-parser"
+import cors from "cors"
+import express from "express"
+import { errorMiddleware } from "../../../packages/error-handler/error-middleware"
 
-const app = express();
+const app = express()
 
-app.use(cors({
-  origin: ["http://localhost:3000"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-  credentials: true,
-}))
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+)
 
-app.use(express.json());
-app.use(cookieParser());
+app.use(express.json())
+app.use(cookieParser())
 
-app.get('/', (_, res) => {
-    res.send({ 'message': 'Hello API'});
-});
-
-app.use(errorMiddleware);
-
-const port = process.env.PORT ? Number(process.env.PORT) : 6001;
-const server = app.listen(port, () => {
-    console.log(`Listening at http://localhost:${port}/api`);
+app.get("/", (_, res) => {
+  res.send({ message: "Hello API" })
 })
 
-server.on('error', (err) => {
-    console.error("Server error:", err);
-});
+app.use(errorMiddleware)
+
+const port = process.env.PORT ? Number(process.env.PORT) : 6001
+const server = app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}/api`)
+})
+
+server.on("error", (err) => {
+  console.error("Server error:", err)
+})
