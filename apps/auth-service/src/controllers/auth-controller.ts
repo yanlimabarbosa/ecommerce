ba8cbdcdd15765579b80de5175684eea5c72fd38
Yan Lima Barbosa <yanbr763@gmail.com>
2025-09-30 00:34:08 -0300

    feat: implement user registration controller with OTP functionality
    - Added a new `auth-controller.ts` file to handle user registration.
    - Implemented validation for registration data and checks for existing users.
    - Integrated OTP management functions to send verification emails and track requests.
    - Enhanced user experience by providing feedback on OTP status.

---
+++ apps/auth-service/src/controllers/auth-controller.ts
@@ -0,0 +1,31 @@
+import type { NextFunction, Request, Response } from "express"
+import { ValidationError } from "../../../../packages/error-handler"
+import prisma from "../../../../packages/libs/prisma"
+import {
+  checkOtpRestrictions,
+  sendOtp,
+  trackOtpRequests,
+  validateRegistrationData,
+} from "../utils/auth.helper"
+
+export const userRegistration = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    validateRegistrationData(req.body, "user")
+
+    const { name, email } = req.body
+
+    const existingUser = await prisma.users.findUnique({ where: email })
+
+    if (existingUser) {
+      return next(new ValidationError("User with this email already exists."))
+    }
+
+    await checkOtpRestrictions(email, next)
+    await trackOtpRequests(email, next)
+    await sendOtp({ name, email, templateName: "user-activation-mail" })
+
+    res.status(200).json({ message: "OTP sent to your email. Please verify your account." })
+  } catch (err) {
+    next(err)
+  }
+}
