d2a47736dcdccc66b1059f62c50baeb9df5ac542
Yan Lima Barbosa <yanbr763@gmail.com>
2025-09-30 00:34:23 -0300

    feat: implement email sending utility for user notifications
    - Added a new `send-mail` utility to handle email sending functionality.
    - Integrated EJS templating for dynamic email content generation.
    - Configured Nodemailer transport settings using environment variables for SMTP.
    - Implemented error handling for email sending operations.

---
+++ apps/auth-service/src/utils/send-mail/index.ts
@@ -0,0 +1,62 @@
+import path from "node:path"
+import dotenv from "dotenv"
+import ejs from "ejs"
+import nodeMailer, { type TransportOptions } from "nodemailer"
+import type Mail from "nodemailer/lib/mailer"
+
+dotenv.config()
+
+type EmailOptions = Mail.Options & Partial<TransportOptions>
+
+type SendEmailParams = {
+  to: string
+  subject: string
+  templateName: string
+  data: Record<string, any>
+}
+
+export const sendEmail = async ({ to, subject, templateName, data }: SendEmailParams) => {
+  try {
+    const html = await renderEmailTemplate(templateName, data)
+
+    const emailOptions: EmailOptions = {
+      from: `<${process.env.SMTP_USER}`,
+      to,
+      subject,
+      html,
+    }
+
+    await transporter.sendMail(emailOptions)
+
+    return true
+  } catch (error) {
+    console.error("Error sending email:", error)
+    return false
+  }
+}
+
+const renderEmailTemplate = async (
+  templateName: string,
+  data: Record<string, any>
+): Promise<string> => {
+  const templatePath = path.join(
+    process.cwd(),
+    "auth-service",
+    "src",
+    "utils",
+    "email-templates",
+    `${templateName}.ejs`
+  )
+
+  return await ejs.render(templatePath, data)
+}
+
+const transporter = nodeMailer.createTransport({
+  host: process.env.SMTP_HOST,
+  port: Number(process.env.SMTP_PORT) || 587,
+  service: process.env.SMTP_SERVICE,
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASSWORD,
+  },
+})
