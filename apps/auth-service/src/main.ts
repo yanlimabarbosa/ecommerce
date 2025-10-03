edfc644258ffb9873b3ad0177cf89a8498e79823
Yan Lima Barbosa <yanbr763@gmail.com>
2025-10-02 22:04:43 -0300

    feat: integrate Swagger UI and routing for Auth Service
    - Added Swagger UI for API documentation access at `/api-docs`.
    - Implemented a new route to serve the Swagger JSON documentation.
    - Integrated the authentication routes into the main application for better organization and accessibility.

--- apps/auth-service/src/main.ts
+++ apps/auth-service/src/main.ts
@@ -1,7 +1,12 @@
 import cookieParser from "cookie-parser"
 import cors from "cors"
 import express from "express"
+import swaggerUi from "swagger-ui-express"
+
 import { errorMiddleware } from "../../../packages/error-handler/error-middleware"
+import router from "./routes/auth.router"
+
+const swaggerDocument = require("./swagger-output.json")
 
 const app = express()
 
@@ -20,11 +25,20 @@ app.get("/", (_, res) => {
   res.send({ message: "Hello API" })
 })
 
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+app.get("/docs-json", (req, res) => {
+  res.json(swaggerDocument)
+})
+
+// Routes
+app.use("/api", router)
+
 app.use(errorMiddleware)
 
 const port = process.env.PORT ? Number(process.env.PORT) : 6001
 const server = app.listen(port, () => {
-  console.log(`Listening at http://localhost:${port}/api`)
+  console.log(`Auth service is running at http://localhost:${port}/api`)
+  console.log(`Swagger Docs available at http://localhost:${port}/docs`)
 })
 
 server.on("error", (err) => {
