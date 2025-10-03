ef5cb20b322c860cd3a2a4dc0b7d24849797eb07
Yan Lima Barbosa <yanbr763@gmail.com>
2025-10-02 22:05:23 -0300

    refactor: improve formatting and organization in main.ts
    - Updated import statements for clarity and consistency.
    - Enhanced formatting of CORS middleware and rate limiting configuration.
    - Standardized spacing and indentation for better readability.

--- apps/api-gateway/src/main.ts
+++ apps/api-gateway/src/main.ts
@@ -1,23 +1,23 @@
-import express from "express"
-import cors from "cors"
-import proxy from "express-http-proxy"
-import morgan from "morgan"
-import rateLimit from "express-rate-limit"
-import swaggerUi from "swagger-ui-express"
 import axios from "axios"
 import cookieParser from "cookie-parser"
-import { Request } from 'express';
+import cors from "cors"
+import express, { type Request } from "express"
+import proxy from "express-http-proxy"
+import rateLimit from "express-rate-limit"
+import morgan from "morgan"
 
 const app = express()
 
-app.use(cors({
-  origin: ["http://localhost:3000"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-  credentials: true,
-}))
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+)
 
 app.use(morgan("dev"))
-app.use(express.json({ limit:"100mb" }))
+app.use(express.json({ limit: "100mb" }))
 app.use(express.urlencoded({ limit: "100mb", extended: true }))
 app.use(cookieParser())
 app.set("trust proxy", 1)
@@ -25,11 +25,11 @@ app.set("trust proxy", 1)
 // Rate limiting
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: (req: Request) => (req.user? 1000 : 100), // limit each IP to 100 requests per windowMs
+  max: (req: Request) => (req.user ? 1000 : 100), // limit each IP to 100 requests per windowMs
   message: "Too many requests from this IP, please try again later.",
   standardHeaders: true,
   legacyHeaders: true,
-  keyGenerator: (req: Request) => req.ip ?? 'unknown', 
+  // keyGenerator: (req: Request) => req.ip ?? "unknown",
 })
 
 app.use(limiter)
