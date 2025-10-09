42ad07b8e44bc4e332363e59c394012125e60402
Yan Lima Barbosa <yanbr763@gmail.com>
2025-10-08 21:48:26 -0300

    feat: add user verification route to auth service
    - Introduced a new POST route for user verification in the auth service.
    - Integrated the verifyUser function from the auth-controller to handle user verification logic.

--- apps/auth-service/src/routes/auth.router.ts
+++ apps/auth-service/src/routes/auth.router.ts
@@ -1,8 +1,9 @@
 import express, { type Router } from "express"
-import { userRegistration } from "../controllers/auth-controller"
+import { userRegistration, verifyUser } from "../controllers/auth-controller"
 
 const router: Router = express.Router()
 
 router.post("/user-registration", userRegistration)
+router.post("/verify-user", verifyUser)
 
 export default router
